0b937e072abf4b1cec036cb0c974120bf18aeb1b
router <messagebus@vk.com>
2022-11-16 04:06:02 +0300

    adjust UI links for server hosting

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -2,12 +2,12 @@
 {
     public static class UILinks
     {
-        public const string GitHub = "https://github.com/space-syndicate/space-station-14/";
+        public const string GitHub = "https://github.com/BasedUser/ss14-ru/tree/clonevax";
         public const string Patreon = "https://boosty.to/corvax";
 
-        public const string Discord = "https://discord.com/invite/F9bNgSYfX5";
-        public const string BugReport = "https://github.com/space-syndicate/space-station-14/issues/new/choose";
-        public const string Website = "https://station14.ru";
-        public const string Wiki = "https://wiki.station14.ru";
+        public const string Discord = "https://discord.ss14.io";
+        public const string BugReport = "https://github.com/BasedUser/ss14-ru/issues/new/choose";
+        public const string Website = "https://spacestation14.io";
+        public const string Wiki = "https://wiki.spacestation14.io"; //:despair:
     }
 }
